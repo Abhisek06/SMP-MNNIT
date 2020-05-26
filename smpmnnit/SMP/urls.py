e839dbe2fe2a429ce4eada2391aad68693e6ced0
abhijeetp94 <abhijeetp94@github.com>
2020-05-26 08:57:18 +0530

    First commit

---
+++ smpmnnit/SMP/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name = "home"),
+    path('about_us/', views.about_us, name = "about_us"),
+    path('general_info/', views.general_info, name = "general_info"),
+    path('academics/', views.academics, name = "academics"),
+    path('campus_life/', views.campus_life, name = "campus_life"),
+    path('extra_curricular/', views.extra_curricular, name = "extra_curricular"),
+    path('questions/', views.questions, name = "questions"),
+    path('departments/', views.departments, name = "departments"),
+    path('login/', views.login, name = "login"),
+    path('contacts/', views.contacts, name = "contacts"),
+    path('details/', views.details, name = "details"), 
+    path('resources/', views.resources, name = "resources"),
+
+]
