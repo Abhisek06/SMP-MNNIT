9c527e3bbce6a0461c7115569c27617aa3d2387d
abhijeetp94 <abhijeetp94@github.com>
2020-05-26 09:22:53 +0530

    commit

--- smpmnnit/SMP/urls.py
+++ smpmnnit/SMP/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+# Pipeline for the website.
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- smpmnnit/SMP/views.py
+++ smpmnnit/SMP/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse("Hello")
+
+def about_us(request):
+    return HttpResponse("Hello")
+
+def general_info(request):
+    return HttpResponse("Hello")
+
+def academics(request):
+    return HttpResponse("Hello")
+
+def campus_life(request):
+    return HttpResponse("Hello")
+
+def extra_curricular(request):
+    return HttpResponse("Hello")
+
+def questions(request):
+    return HttpResponse("Hello")
+
+def details(request):
+    return HttpResponse("Hello")
+
+def departments(request):
+    return HttpResponse("Hello")
+
+def contacts(request):
+    return HttpResponse("Hello")
+
+def resources(request):
+    return HttpResponse("Hello")
